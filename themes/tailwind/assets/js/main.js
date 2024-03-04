516eda98e45e3da869ade7e5d5e7aa9987a29bf0
Le Quoc Viet <vietlq4@Les-Mac-mini.lan>
2024-03-04 23:41:32 +0700

    add fb comment

--- themes/tailwind/assets/js/main.js
+++ themes/tailwind/assets/js/main.js
@@ -40,4 +40,46 @@
       navbarLang && navbarLang.classList.add('hidden');
     }
   });
+
+  const indicator = document.querySelector('.nav-indicator');
+	const items = document.querySelectorAll('.nav-item');
+
+	function handleIndicator(el) {
+		items.forEach(item => {
+			item.classList.remove('is-active');
+			item.removeAttribute('style');
+		});
+
+		indicator.style.width = `${el.offsetWidth}px`;
+		indicator.style.left = `${el.offsetLeft}px`;
+		indicator.style.backgroundColor = el.getAttribute('active-color');
+
+		el.classList.add('is-active');
+		el.style.color = el.getAttribute('active-color');
+	}
+
+
+	items.forEach((item, index) => {
+		item.addEventListener('click', (e) => { handleIndicator(e.target) });
+		item.classList.contains('is-active') && handleIndicator(item);
+	});
+
+    // get button element
+	const disqusBtn = document.getElementById("disqus-btn");
+	const fbBtn = document.getElementById("fb-btn");
+
+	// get message board element
+	const disqusBoard = document.getElementById("disqus-comments");
+	const fbBoard = document.getElementById("facebook-comments");
+	// Add click event listeners to each button
+
+	disqusBtn.addEventListener("click", function () {
+		disqusBoard.style.display = "block";
+		fbBoard.style.display = "none";
+	});
+	fbBtn.addEventListener("click", function () {
+		disqusBoard.style.display = "none";
+		fbBoard.style.display = "block";
+	});
+
 })();
